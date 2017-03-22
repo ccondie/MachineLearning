53f6ee3cf43f319a6907c644ca0bd446916491d1
ccondie <clayton.condie@gmail.com>
2017-03-22 00:21:53 -0600

    basic knn in place, nominal, no weights

--- toolkit/knn.py
+++ toolkit/knn.py
@@ -1,26 +1,93 @@
 from supervised_learner import SupervisedLearner
 import math
+import operator
+from matrix import Matrix
 
 
 class InstanceBasedLearner(SupervisedLearner):
     def __init__(self):
         self.weightFlag = False
+        self.regressFlag = False
         self.k = 3
         self.instances = None
         self.classes = None
+        self.predict_count = 0
+        self.train_max_size = 2000
 
     def train(self, instances, classes):
-        # NOT SURE THIS IS ACTUALLY NEEDED
-        self.instances = instances
-        self.classes = classes
+        self.instances = Matrix(instances, 0, 0, 0, instances.cols)
+        self.classes = Matrix(classes, 0, 0, 0, classes.cols)
 
-    def predict(self, instances, classes):
+        instances.shuffle(classes)
 
-        pass
+        # build the train set
+        if instances.rows < self.train_max_size:
+            instance_count = instances.rows
+        else:
+            instance_count = self.train_max_size
 
-    def dist(self, inst1, inst2):
-        for row_index in range(inst1.rows):
-            row = inst1.row(row_index)
-            for col_index in range(inst1.cols):
-                print(str(row[col_index]) + ':' + str(inst1.value_count(col_index)), end=', ')
-            print()
+        for i in range(instance_count):
+            self.instances.add_row(instances, i, 0, instances.cols)
+            self.classes.add_row(classes, i, 0, classes.cols)
+
+    def predict(self, instance, classif):
+        print(self.predict_count)
+        self.predict_count += 1
+        # distance_map[train_index] = distance_value
+        distance_map = dict()
+        # calculate the distances from the inputted distance ... map each distance to train_instance index
+        for train_index in range(self.instances.rows):
+            distance_map[train_index] = self.dist(instance, self.instances.row(train_index))
+
+        # sort the distance map, index 0 is smallest distance
+        distance_sorted = sorted(distance_map.items(), key=operator.itemgetter(1))
+
+        if self.classes.value_count(0) == 0:
+            # classification is continuous
+
+            # set the "return" value
+            if len(classif) == 0:
+                classif.append(0)
+            else:
+                classif[0] = 0
+        else:
+            # classification is nominal
+            vote_map = dict()
+            for vote_index in range(self.k):
+                # classification is nominal
+                # vote_map[nominal_value] = vote_count
+                if self.weightFlag:
+                    # weighted
+                    nominal_value = self.classes.row(distance_sorted[vote_index][0])
+                    if nominal_value in vote_map:
+                        vote_map[nominal_value] += 1 / math.pow(distance_sorted[vote_index][1], 2)
+                    else:
+                        vote_map[nominal_value] = 1 / math.pow(distance_sorted[vote_index][1], 2)
+                else:
+                    # unweighted
+                    nominal_value = self.classes.row(distance_sorted[vote_index][0])[0]
+                    if nominal_value in vote_map:
+                        vote_map[nominal_value] += 1
+                    else:
+                        vote_map[nominal_value] = 1
+            # sort the votes and return the nominal value with the most votes
+            vote_sorted = sorted(vote_map.items(), key=operator.itemgetter(1))
+            # set the "return" value
+            if len(classif) == 0:
+                classif.append(vote_sorted[-1][0])
+            else:
+                classif[0] = vote_sorted[-1][0]
+
+    def dist(self, test_row, train_row):
+        sum_value = 0
+        for col_index in range(len(train_row)):
+            if self.instances.value_count(col_index) == 0:
+                # value is continuous
+                sum_value += math.pow(train_row[col_index] - test_row[col_index], 2)
+            else:
+                # value is nominal
+                if test_row[col_index] == train_row[col_index]:
+                    sum_value += 0
+                else:
+                    sum_value += 1
+        return math.sqrt(sum_value)
